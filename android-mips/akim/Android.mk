5942c41fc59b58651d7f3e281ab7ed314b5f5800
Chris Dearman <chris.dearman@imgtec.com>
2013-05-28 16:29:24 -0700

    [MC] Install prebuilt version of libakim.so for MIPS
    Change-Id: Ib4b2730bdf831a69e200c1bf4088a333b3269376

---
+++ android-mips/akim/Android.mk
@@ -0,0 +1,18 @@
+ifeq ($(TARGET_ARCH),mips)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+ifneq ($(findstring mips32r2,$(TARGET_ARCH_VARIANT)),)
+LOCAL_SRC_FILES := mips32r2/libakim.so
+else
+LOCAL_SRC_FILES := mips32r1/libakim.so
+endif
+
+LOCAL_MODULE := libakim.so
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := shell_$(TARGET_SHELL)
+include $(BUILD_PREBUILT)
+
+endif
